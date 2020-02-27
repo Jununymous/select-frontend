0e28e98a1a7fe665a1cddbe3deba5b14d5b79d09
JunWoo <junu.sinclair@gmail.com>
2020-02-28 00:38:16 +0900

    인기 아티클 리듀서 및 saga 관련 추가.

---
+++ src/app/services/articlePopular/sagas.ts
@@ -0,0 +1,33 @@
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+
+import { Actions } from 'app/services/articlePopular';
+import { Actions as ArticleActions } from 'app/services/article';
+import {
+  PopularArticleListResponse,
+  requestPopularArticleList,
+} from 'app/services/articlePopular/requests';
+
+export function* loadPopularArticlesRequest({
+  payload,
+}: ReturnType<typeof Actions.loadPopularArticlesRequest>) {
+  const { page } = payload;
+  try {
+    const response: PopularArticleListResponse = yield call(requestPopularArticleList, { page });
+    yield put(
+      ArticleActions.updateArticles({
+        articles: response.articles.map(data => data.article),
+      }),
+    );
+    yield put(Actions.afterLoadPopularArticles({ page, response }));
+  } catch {
+    yield put(Actions.afterLoadPopularArticles({ page }));
+  }
+}
+
+export function* watchLoadPopularArticlesRequest() {
+  yield takeLatest(Actions.loadPopularArticlesRequest.getType(), loadPopularArticlesRequest);
+}
+
+export function* articlePopularRootSaga() {
+  yield all([watchLoadPopularArticlesRequest]);
+}
