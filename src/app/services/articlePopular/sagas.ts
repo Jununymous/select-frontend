f2caea4db8a22c777a688f459516284fc5dc9996
JunWoo <junu.sinclair@gmail.com>
2020-02-28 01:10:27 +0900

    아티클 홈 화면에서 인기 아티클 목록 호출하도록 수정.
    # Conflicts:
    #	src/app/components/ArticleHome/ArticleHomeSection.tsx

--- src/app/services/articlePopular/sagas.ts
+++ src/app/services/articlePopular/sagas.ts
@@ -1,11 +1,12 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
 
-import { Actions } from 'app/services/articlePopular';
-import { Actions as ArticleActions } from 'app/services/article';
+import { Actions as ArticleActions } from 'app/services/articlePopular';
+import { Actions } from 'app/services/article';
 import {
   PopularArticleListResponse,
   requestPopularArticleList,
 } from 'app/services/articlePopular/requests';
+import toast from 'app/utils/toast';
 
 export function* loadPopularArticlesRequest({
   payload,
@@ -14,20 +15,21 @@ export function* loadPopularArticlesRequest({
   try {
     const response: PopularArticleListResponse = yield call(requestPopularArticleList, { page });
     yield put(
-      ArticleActions.updateArticles({
+      Actions.updateArticles({
         articles: response.articles.map(data => data.article),
       }),
     );
-    yield put(Actions.afterLoadPopularArticles({ page, response }));
+    yield put(ArticleActions.afterLoadPopularArticles({ page, response }));
   } catch {
-    yield put(Actions.afterLoadPopularArticles({ page }));
+    toast.failureMessage('인기 아티클 목록을 불러오는데 실패했습니다.');
+    yield put(ArticleActions.afterLoadPopularArticles({ page }));
   }
 }
 
 export function* watchLoadPopularArticlesRequest() {
-  yield takeLatest(Actions.loadPopularArticlesRequest.getType(), loadPopularArticlesRequest);
+  yield takeLatest(ArticleActions.loadPopularArticlesRequest.getType(), loadPopularArticlesRequest);
 }
 
 export function* articlePopularRootSaga() {
-  yield all([watchLoadPopularArticlesRequest]);
+  yield all([watchLoadPopularArticlesRequest()]);
 }
